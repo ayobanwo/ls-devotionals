8d2299a2e915a1622ee383485ce0cd2307771fbe
ayobanwo <olubanwoayomide@gmail.com>
2022-08-25 03:10:31 +0100

    new commit

---
+++ admin/script.js
@@ -0,0 +1,64 @@
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    let body = document.querySelector('#body').value;
+    let title = document.querySelector('#title').value;
+    let day = document.querySelector('#day').value;
+    let bible_text = document.querySelector('#bible_text').value;
+    let prayer = document.querySelector('#prayer').value;
+    let further_study = document.querySelector('#further_study').value;
+    let am_scripture = document.querySelector('#am_scripture').value;
+    let pm_scripture = document.querySelector('#pm_scripture').value;
+    let week_teaching = document.querySelector('#week_teaching').value;
+    let image_url = document.querySelector('#image_url').value;
+
+    dataObj = {};
+
+    dataObj['body'] = body;
+    dataObj['title'] = title;
+    dataObj['day'] = day;
+    dataObj['bible_text'] = bible_text;
+    dataObj['prayer'] = prayer;
+    dataObj['further_study'] = further_study;
+    dataObj['am_scripture'] = am_scripture;
+    dataObj['pm_scripture'] = pm_scripture;
+    dataObj['week_teaching'] = week_teaching;
+    dataObj['image_url'] = image_url;
+
+    console.log(dataObj);
+
+    // fetch("https://dailydevotionals.herokuapp.com/api/post/create.php", {
+    //     method: 'post',
+    //     body: JSON.stringify(dataObj),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(function (response){
+    //     return response.json();
+    // }).then(function (responseData){
+    //     let message = responseData.message;
+    //     console.log(message);
+    //     console.log(responseData);
+    //   return message;
+    // }).catch(function (error){
+    //     console.log(error);
+    // })
+
+    fetch(" https://dailydevotionals.herokuapp.com/api/get/getAll.php")
+        .then((response) => response.json())
+        .then((data) => {
+            const arr = data.data
+
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i].day === '2022-09-01') {
+                    todayContent = arr[i];
+                }
+            }
+        }).catch(function (error){
+            console.log(error);
+        })
+
+    console.log(result);
+})
---
+++ js/content.js
@@ -0,0 +1,44 @@
+
+fetch(" https://dailydevotionals.herokuapp.com/api/get/getAll.php")
+    .then((response) => response.json())
+    .then((data) => {
+        const arr = data.data
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = yyyy + '-' + mm+ '-' + dd;
+        today = '2022-09-01'
+        console.log(today);
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].day === today) {
+                todayContent = arr[i];
+            }
+        }
+        let body = document.querySelector('#body')
+        let title = document.querySelector('#title')
+        let day = document.querySelector('#day')
+        let bible_text = document.querySelector('#bible_text')
+        let prayer = document.querySelector('#prayer')
+        let further_study = document.querySelector('#further_study')
+        let am_scripture = document.querySelector('#am_scripture')
+        let pm_scripture = document.querySelector('#pm_scripture')
+        let week_teaching = document.querySelector('#week_teaching')
+        let image_url = document.querySelector('#image_url')
+
+
+        body.innerHTML = todayContent.body
+        title.innerHTML = todayContent.title
+        // day.innerHTML = todayContent.day
+        bible_text.innerHTML = todayContent.bible_text
+        prayer.innerHTML = todayContent.prayer
+        further_study.innerHTML = todayContent.further_study
+        am_scripture.innerHTML = todayContent.am_scripture
+        pm_scripture.innerHTML = todayContent.pm_scripture
+        week_teaching.innerHTML = todayContent.week_teaching
+
+    }).catch(function (error){
+        console.log(error);
+    })
