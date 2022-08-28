2abb3597948cbf626509e7feb6d55b60ddc9fd9e
ayobanwo <olubanwoayomide@gmail.com>
2022-08-28 22:56:39 +0100

    logo

--- js/content.js
+++ js/content.js
@@ -9,7 +9,7 @@ fetch(" https://dailydevotionals.herokuapp.com/api/get/getAll.php")
         var yyyy = today.getFullYear();
 
         today = yyyy + '-' + mm+ '-' + dd;
-        
+        today = '2022-09-02'
         for (let i = 0; i < arr.length; i++) {
             if (arr[i].day === today) {
                 todayContent = arr[i];
